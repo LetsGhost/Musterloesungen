34a431da5867751188af693d9bc7f47f423f6e12
Paul Ehrling <ghostlest330@gmail.com>
2024-06-12 10:17:29 +0200

    feat: Initialize Express server with JSON body parsing middleware

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+// Initializes the .env file
+dotenv.config();
+
+import connectToDatabase from './configs/mongodb.js';
+
+// Connect to the database
+connectToDatabase();
+
+const server = express();
+
+// Use bodyParser middleware to parse incoming JSON payloads, making them available under req.body in route handlers
+server.use(bodyParser.json());
+
+// Start the server on the specified port
+server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
